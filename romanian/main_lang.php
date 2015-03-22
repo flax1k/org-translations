f9cd581503ef915aece2865d3d09cf0ba2e3d7a0
flax1k <flavius.cristiu@gmail.com>
2015-03-22 19:44:46 +0200

    Update main_lang.php

--- romanian/main_lang.php
+++ romanian/main_lang.php
@@ -1,1 +1,89 @@
+<?php
+//Menu/Page items
+$lang['global_home']            = "Acasa";
+$lang['global_discover']        = "Descopera";
+$lang['global_store']           = "Magazin";
+$lang['global_journal']         = "Jurnal";
+$lang['global_support']         = "Suport";
+$lang['global_developer']       = "Dezvoltator";
+$lang['global_getinvolved']     = "Implica-te";
+$lang['global_profile']         = "Profil";
+$lang['global_search']          = "Cauta";
+$lang['global_docs']            = "Docs";
+$lang['global_settings']        = "Setari";
+$lang['global_donate']        	= "Doneaza";
+$lang['global_code of conduct'] = "Cod de conduita";
 
+//Footer
+$lang['global_changelang']      = "Schimba limba site-ului";
+$lang['global_content_license'] = "Continut licentiat sub:";
+$lang['global_team']            = "Echipa";
+$lang['global_terms']           = "Termeni de utilizare";
+$lang['global_user_guide']      = "Ghid utilizator";
+$lang['global_answers']         = "Raspunsuri";
+$lang['global_user_chat']       = "Chat utilizator";
+$lang['global_dev_chat']        = "Chat dezvoltator";
+$lang['global_design']          = "Design";
+$lang['global_code']            = "Cod";
+$lang['global_manage']          = "Administreaza";
+$lang['global_distribute']      = "Distribuie";
+
+$lang['global_news']            = 'Noutati';
+$lang['global_dev']             = 'Dezvoltator';
+$lang['global_edtrl']           = 'Editorial';
+$lang['global_intv']            = 'Interviu';
+$lang['global_mini']            = 'Mini';
+$lang['global_ux']              = 'Experinta utilizator';
+$lang['global_video']           = 'Video';
+
+$lang['global_team_web']        = 'Web';
+$lang['global_team_press']      = 'Presa';
+$lang['global_team_ux']         = 'Design';
+$lang['global_team_mod']        = 'Moderator';
+$lang['global_team_dev']        = 'Dezvoltator';
+$lang['global_team_core']       = 'Consiliu';
+
+$lang['global_login_header']    = "Doar o secunda";
+$lang['global_login_text']      = "Logheaza-te pentru a comenta, a pune intrebari si multe altele.";
+$lang['global_forgot_password'] = "Ai uitat parola?";
+$lang['global_username']        = "Nume utilizator";
+$lang['global_password']        = "Parola";
+$lang['global_jsnotice']        = "Permite JavaScript in browserul tau pentru o experinta uimitoare pe elementaryos.org";
+$lang['global_comment']         = "Comentariu";
+$lang['global_comments']        = "Comentarii";
+$lang['global_view_profile']	= "Vizualizeaza profil.";
+$lang['global_load_more']		= "Incarca mai multe";
+
+//actions
+$lang['global_login']           = "Login";
+$lang['global_logout']          = "Logout";
+$lang['global_register']        = "Inregistreaza-te";
+$lang['global_status']	        = "Status";
+$lang['global_save']            = "Salveaza";
+$lang['global_edit'] 			= "Editare";
+$lang['global_delete'] 			= "Sterge";
+$lang['global_cancel']          = "Anuleaza";
+$lang['global_reply']           = "Raspunde";
+$lang['global_report']          = "Raporteaza";
+
+//Strings that don't need there own file, 
+// no need to put extra load on the system for one string
+$lang['global_email']           = 'Email';
+$lang['write_comment']          = 'Scrie un comentariu&#8230;';
+$lang['search_results']         = 'Rezultatele cautarii';
+$lang['error_gohome']           = 'mergi la pagina principala';
+$lang['error_404_title']        = 'Pagina pe care o cauti nu poate fi gasita';
+$lang['error_404_descr']        = "Nu este nimic la \"%s\". Verifica adresa de web pentru litere/cuvinte gresite si incearca din nou."; //%s = page url without domain
+$lang['error_403_title']        = 'Nu ai acces sa vezi aceasta pagina';
+$lang['error_403_descr']        = 'Accesul la aceasta pagina este permis doar utilizatorilor autorizati.';
+$lang['error_500_title']        = 'A aparu o problema la deschiderea paginii';
+$lang['error_500_descr']        = 'Ceva a merg gresit cand s-a incercat accesarea paginii. Incearca mai tarziu.';
+$lang['forgot_password_title']  = 'Ai uitat parola?';
+$lang['forgot_password_sub']    = 'Se intampla celor mai buni dintre noi.';
+$lang['forgot_password_req']    = 'Cere o noua parola';
+$lang['password_reset']         = "Parola ta a fost resetata";
+
+
+$lang['comment_none_found']     = "Acest/Acesta %s nu are inca niciun comentariu."; //%s = type. Anwser, Article, Question
+$lang['comment_login']     		= "<a href='%s'>Logheaza-te</a> pentru a putea comenta."; //%s = full url to login page
+$lang['comment_banned']    		= "Ai fost banat. Nu mai poti comenta.";
