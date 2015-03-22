438ce1344b5ddf1b6e899fc47a3746b221e56ae4
flax1k <flavius.cristiu@gmail.com>
2015-03-22 19:16:32 +0200

    Update team_lang.php

--- romanian/team_lang.php
+++ romanian/team_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['team_title']                     = "Echipa noastra";
-$lang['team_subtitle']                  = "Iti ia un sat sa construiesti un sistem de operare";
+$lang['team_subtitle']                  = "Iti ia un sat intreg sa construiesti un sistem de operare";
 $lang['team_mod_descr']                 = "Campionii Justitiei. Aparatorii Pacii. Aceasta echipa de moderatori dedicati exista pentru a proteja si a servi.";
 $lang['team_core_descr']                = "O echipa pentru a-i conduce pe toti. O echipa pentru a-i conecta. O echipa pentru a-i aduce pe toti, iar in intuneric sa-i conecteze.";
 $lang['team_press_descr']               = "Mereu grabit, interconectat, inaintea curbei, si impingandu-te spre marginea scaunului.";
