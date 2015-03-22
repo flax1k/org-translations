978e63221447d103c0a6d0111613d36507a709ce
flax1k <flavius.cristiu@gmail.com>
2015-03-22 19:16:02 +0200

    Update team_lang.php

--- romanian/team_lang.php
+++ romanian/team_lang.php
@@ -1,1 +1,11 @@
+<?php
+
+$lang['team_title']                     = "Echipa noastra";
+$lang['team_subtitle']                  = "Iti ia un sat sa construiesti un sistem de operare";
+$lang['team_mod_descr']                 = "Campionii Justitiei. Aparatorii Pacii. Aceasta echipa de moderatori dedicati exista pentru a proteja si a servi.";
+$lang['team_core_descr']                = "O echipa pentru a-i conduce pe toti. O echipa pentru a-i conecta. O echipa pentru a-i aduce pe toti, iar in intuneric sa-i conecteze.";
+$lang['team_press_descr']               = "Mereu grabit, interconectat, inaintea curbei, si impingandu-te spre marginea scaunului.";
+$lang['team_dev_descr']                 = "Somnul este pentru cei slabi. Ei mananca in termeni de megabiti,vorbesc in algoritmi si sangereaza binar.";
+$lang['team_web_descr']                 = "Traiesc printre nori, impletind panze nesfarsite pentru compatibilitati ale browserelor. Ce tu visezi, ei creaza.";
+$lang['team_design_descr']              = "Numeste-i obsedati compulsivi, orientati spre detaliu, sau pur si simplu cei care muta pixelii. Designul nu este ceea ce fac, este ceea ce sunt.";
 
