9182ae99d566e3c5afc2847430f52bac65e949ac
flax1k <flavius.cristiu@gmail.com>
2015-03-22 18:38:51 +0200

    Create account_lang.php

---
+++ romanian/account_lang.php
@@ -0,0 +1,31 @@
+<?php
+$lang['account_emailaddr'] 		= "Adresa de email";
+$lang['account_showemail'] 		= "Afiseaza adresa mea de email in profil";
+$lang['account_firstname'] 		= "Prenume";
+$lang['account_lastname'] 		= "Nume";
+$lang['account_name'] 		    = "Nume cont";
+$lang['account_ircnick'] 		= "IRC Nickname";
+$lang['account_nick'] 			= "nume utilizator";
+$lang['account_newpasswd'] 		= "Parola Noua";
+$lang['account_desiredpasswd'] 	= "Parola dorita";
+$lang['account_desirednick'] 	= "Nume de utilizator dorit";
+$lang['account_passwdagain'] 	= "Confirma parola";
+$lang['account_location'] 		= "Locatia";
+$lang['account_sitelang'] 		= "Limba site-ului";
+$lang['account_chooseavatar'] 	= "Alege imagine avatar";
+$lang['account_settings'] 		= "Setarile contului";
+
+$lang['account_forgotpasswd'] 	= 'ai uitat parola';
+$lang['account_join']		  	= "Alatura-te %d utilizatori ca tine"; //%d in numarul utilizatorilor
+
+$lang['account_already_got'] 	= 'Ai deja un cont?';
+
+$lang['account_no_acc']			= "Nu ai inca un cont?";
+
+//activate page
+$lang['account_resend']			= "Retrimite";
+$lang['account_no_activate']	= "N-ai primit niciun email?";
+$lang['account_verify']			= "Verifica";
+$lang['account_verify_code']	= "Cod De Verificare";
+$lang['account_please_verify']	= "Verificati casuta de email pentru codul de verificare si introduceti-l aici.";
+$lang['account_verify_sent']	= "Confirmarea nu a fost trimisa la %s"; // %s este adresa ta de email, please always include it
