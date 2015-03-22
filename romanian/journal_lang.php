cde8fb872321b5cfd256f6846c51300608467c4c
flax1k <flavius.cristiu@gmail.com>
2015-03-22 19:46:13 +0200

    Update journal_lang.php

--- romanian/journal_lang.php
+++ romanian/journal_lang.php
@@ -1,1 +1,7 @@
 
+<?php
+$lang['journal_loadmore']				= "Incarca mai multe";
+$lang['journal_newsandupdates']			= "Noutati &amp; Update-uri";
+$lang['journal_noarticles']				= "Niciun articol gasit.";
+$lang['journal_by']						= "de";
+$lang['journal_readmore']				= "Citeste mai mult";
