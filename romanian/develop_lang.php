37f06411199c1cf5f82d340dcf611c9ba94036e4
flax1k <flavius.cristiu@gmail.com>
2015-03-22 18:55:13 +0200

    Create develop_lang.php

---
+++ romanian/develop_lang.php
@@ -0,0 +1,9 @@
+<?php
+$lang['develop_title']                  = "Dezvolta";
+$lang['develop_subtitle']               = "Ideile tale in linii de cod";
+$lang['develop_parahead']               = "Resurse, Informatii & Mai Multe";
+$lang['develop_paragraph']              = "Construit pe cea mai buna tehnologie open source, elementary este un is o platforma dezvoltator extrem de prietenoasa. Oferim dezvoltatorilor existenti si potentiali cele mai bune resurse si documentatii pentru a fi siguri ca aplicatiile tale intr-adevar stralucesc.";
+$lang['develop_link1']                  = "Citeste Interfata umana a codului de conduita(HIG) si invata cum sa-ti faci aplicatiile sa fie experiente de prima clasa.";
+$lang['develop_link2']                  = "Invata despre Interfata de Programare a Aplicatiilor (API), si descopera toate tehnologiile incluse pregatite pentru tine.";
+$lang['develop_link3']                  = "Foloseste Launchpad pentru a administra rapoartele de bug-uri si pentru a colabora cu alti dezvoltatori.";
+$lang['develop_link4']                  = "Seteaza un cont Ubuntu Aplicatiile Mele si publica aplicatiile tale in Centrul Software.";
