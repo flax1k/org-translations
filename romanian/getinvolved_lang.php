68da727f110cf69e79df0e7fdcf53c8963567b3f
flax1k <flavius.cristiu@gmail.com>
2015-03-23 19:11:39 +0200

    Update getinvolved_lang.php

--- romanian/getinvolved_lang.php
+++ romanian/getinvolved_lang.php
@@ -1,1 +1,35 @@
+<?php
+// Overall sentence reads "Help build the OS of Tomorrow" but Tomorrow is on a new line with different styling
+$lang['gi_main_heading1p1']             = "Ajuta la construirea sistemului de operare al";
+$lang['gi_main_heading1p2']             = "Zilei de maine";
 
+$lang['gi_heading1']                    = "Desktop";
+$lang['gi_heading2']                    = "Design";
+$lang['gi_heading3']                    = "Traduceri";
+$lang['gi_heading4']                    = "Web";
+
+$lang['gi_sec1para1']                   = "Mediul nostru desktop și toate aplicațiile sale sunt construite folosind Vala, GTK+, Clutter, Cairo, Granite, precum și un numar de alte biblioteci gratuite. Toate codurile noastre sunt gazduite pe Launchpad.net, un serviciu gratuit de proiecte open source. Suntem mereu in cautarea de colaboratori de toate nivelele de calificare pentru a ajuta la bug-uri si introducerea de noi caracteristici.";
+$lang['gi_sec1link1']                   = "Navigheza prin codul nostru pentru Desktop";
+$lang['gi_sec1link2']                   = "Navigheza prin codul nostru pentru aplicatii";
+$lang['gi_sec1link3']                   = "Vizualizeaza rapoartele noastre de bug-uri";
+$lang['gi_sec1link4']                   = "Citeste lista noastre de emailuri";
+
+$lang['gi_sec2para1']                   = "Fiecare proiect incepe cu o idee. Echipa noastre de Design ia ideile si le schimba in itinerare pe harta. Noi despartim design-ul in doua componente:";
+$lang['gi_sec2subpara1']                = "Un mod grozav pentru a incepe pentru designerii vizuali este partajarea machetelor cu grupul nostru deviantART.";
+$lang['gi_sec2subpara2']                = "Folosim un sistem in Launchpad denumit Blueprints(Planuri) pentru a crea explicatii detaliate ale noilor caracteristici.";
+$lang['gi_sec2subhead1']                = "Design Vizual.";
+$lang['gi_sec2subhead2']                = "Design Interactiv.";
+$lang['gi_sec2link1']                   = "Vizualizeaza grupul nostru deviantART";
+$lang['gi_sec2link2']                   = "Navigheaza prin Browse our Planuri";
+
+$lang['gi_sec3para1']                   = "elementary este creat si folosit de oameni din intreaga lume; ajuta-ne sa facem experienta oamenilor mai buna prin traducerea in mai multe limbi. Launchpad are incorporata o unealta denumita Rosetta, care permite traduceri colaborative online.";
+$lang['gi_sec3para2']                   = "De asemenea, lucram la traducerea intregului continut al web site-ului nostru in mai multe limbi. Aceasta nu este inca disponibil inca, dar stai conectat pentru mai multe informatii.";
+$lang['gi_sec3link1']                   = "Primeste mai multe informatii despre Rosetta";
+$lang['gi_sec3link2']                   = "Vizualizeaza pagina noastra de traduceri";
+
+$lang['gi_sec4para1']                   = "Website-ul nostru este construit folosind HTML, CSS, PHP, MySQL, si JavaScript. Cautam mereu persoane care au experienta in acele arii si care ar dori sa contribuie pentru a-l face mai bun. Cea mai mare parte a muncii de design este facuta de echipa noastra de design, dar ne place sa primim idei si pareri pentru echipa de design.";
+
+$lang['gi_irclink']                     = "Alatura-te noua in Internet Relay Chat (IRC)";
+$lang['gi_web_report_bug'] = "Raporteaza un bug al website-ului";
+$lang['gi_web_translations'] = "Ajuta la traducerea acestui website";
+$lang['gi_view_design_submission'] = "Vizualizeaza prezentarea noastra Cum sa...";
